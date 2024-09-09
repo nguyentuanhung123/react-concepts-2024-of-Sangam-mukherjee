93a56586bccbafb0d219a0795e2b70669cdaf5cd
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-09-09 10:34:03 +0700

    conditional statements on productList

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -4,13 +4,42 @@ import ProductItem from "./components/product-item";
 import './style.css';
 
 const ProductList = ({ name, city, listOfProducts }) => {
+
+    const flag = true;
+
+    // const renderTextBlock = (getFlag) => {
+    //     return getFlag ? (
+    //         <h4>Name is {name}, he/she is belongs to this city {city}</h4>
+    //     ) : (
+    //         <h4>Hello World</h4>
+    //     )
+    // }
+
+
+    // const renderTextBlock = flag ? (
+    //     <h4>Name is {name}, he/she is belongs to this city {city}</h4>
+    // ) : (
+    //     <h4>Hello World</h4>
+    // )
+
+    let renderTextBlock = null;
+
+    if(flag) {
+        renderTextBlock = 
+            <h4>Name is {name}, he/she is belongs to this city {city}</h4>
+    } else {
+        renderTextBlock = 
+            <h4>Hello World</h4>
+    }
     
     return (
         <div>
             <h3 className="title">ECommer Project</h3>
             {/* <ProductItem /> */}
-            <h4>Name is {name}, he/she is belongs to this city {city}</h4>
-
+            {
+                // renderTextBlock(flag)
+                renderTextBlock
+            }
             {
                 listOfProducts.map((item, index) => (
                     <ProductItem singleProductItem={item} key={index}/>
